482164bcf3ef2df366d7b2907bed9a48f465057a
Developer <developer@local.dev>
2026-03-20 10:45:00 +0100

    test(frontend): configure Jest and React Testing Library

---
+++ frontend/jest.config.js
@@ -0,0 +1,28 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: ['**/*.test.js'],
+  collectCoverageFrom: [
+    'app/**/*.js',
+    'components/**/*.js',
+    '!app/layout.js',
+    '!**/*.test.js',
+  ],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
